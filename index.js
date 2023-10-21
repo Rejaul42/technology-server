9a7ba8e9d9a2493e69d3ce0d5c1c0ee522500663
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-10-21 04:20:28 -0600

    read data from the database

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ async function run() {
         res.send(result)
     })
 
+    // get data from database
+    app.get("/product", async(req, res) =>{
+        const cursor = productCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
